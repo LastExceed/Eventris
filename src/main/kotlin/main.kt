7ec3afa4b7a6eaafbb592cc0d37e6948ad7af235
lastexceed <chris20194@googlemail.com>
2021-05-18 18:11:56 +0000

    remove leftover comment

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,21 +2,3 @@ fun main() {
 	Renderer.init()
 	Renderer.runEventLoop()
 }
-
-//kick table
-
-
-//fun triangle() {
-//	glClear(GL_COLOR_BUFFER_BIT)
-//
-//	glBegin(GL_TRIANGLES)
-//	glColor3f(1f, 0f, 0f)
-//	glVertex2d(-1.0, -1.0)
-//	glColor3f(0f, 1f, 0f)
-//	glVertex2d(1.0, -1.0)
-//	glColor3f(0f, 0f, 1f)
-//	glVertex2d(0.0, 1.0)
-//	glEnd()
-//
-//	window.swapBuffers()
-//}
